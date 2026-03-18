9202b2b045795e6458ab788535fa6ba7d027da33
rebecalira40-hub <rebecalira40@gmail.com>
2026-03-18 10:46:34 -0300

    Update CartasSuperTrunfo.c teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,40 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado;
+  char codigocarta [4];
+  int populacao;
+  float area;
+  float pib;
+  int pontosturisticos;
+  
   // Área para entrada de dados
+  printf("Digite seu Estado: \n");
+  scanf("%c", &estado);
 
+  printf("Digite seu Codigo da Carta: \n");
+  scanf("%c", &codigocarta);
+
+  printf("Digite a População do Estado: \n");
+  scanf("%d", &populacao);
+
+  printf("Digite a Area do Estado: \n");
+  scanf("%f", &area);
+
+  printf("Digite o Pib do Estado: \n");
+  scanf("%f", &pib);
+
+  printf("Digite a quantidade de pontos turísticos: \n");
+  scanf("&d", &pontosturisticos);
+  
   // Área para exibição dos dados da cidade
+
+  printf("Seu Estado é: %c\n", estado);
+  printf("Seu Codigo da Carta é: %c\n", codigocarta);
+  printf("Sua população é: %d\n", populacao);
+  printf("Sua area é: %f\n", area);
+  printf("Seu PIB é: %f\n", pib);
+  printf("A quantidade de pontos turisticos é: %d", pontosturisticos);
 
 return 0;
 } 
